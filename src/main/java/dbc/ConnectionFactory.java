188e59c3bca39002aa72b3ddb4d55e9c86aa4c7d
ShiroNatsu22 <aushojgs@gmail.com>
2017-02-04 18:54:52 +0100

    usersMYSQL acabat

--- src/main/java/dbc/ConnectionFactory.java
+++ src/main/java/dbc/ConnectionFactory.java
@@ -9,11 +9,11 @@ public class ConnectionFactory {
     private static ConnectionFactory instance = new ConnectionFactory();
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://192.168.1.45/mysql";
+    static  String DB_URL;
 
     //  Database credentials
-    static final String USER = "root";
-    static final String PASS = "test";
+    static  String USER;
+    static  String PASS;
 
     private ConnectionFactory() {
         try {
@@ -37,66 +37,11 @@ public class ConnectionFactory {
         return instance.createConnection();
     }
 
-
-   /* public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
-        try {
-            //STEP 2: Register JDBC driver
-            Class.forName(JDBC_DRIVER);
-
-            //STEP 3: Open a connection
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-            //STEP 4: Execute a query
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-            String sql;
-
-            sql="SELECT user.Host,user.User FROM mysql.user";
-            ResultSet rs = stmt.executeQuery(sql);
-
-            //STEP 5: Extract data from result set
-            while (rs.next()) {
-
-                String uname = rs.getString("user.User");
-                String rol = rs.getString("user.Host");
-
-
-                //Display values
-                System.out.println("User: " + uname);
-                System.out.println("rol: " + rol);
-
-
-
-            }
-            //STEP 6: Clean-up environment
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch (SQLException se) {
-            //Handle errors for JDBC
-            se.printStackTrace();
-        } catch (Exception e) {
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null)
-                    stmt.close();
-            } catch (SQLException se2) {
-            }// nothing we can do
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        System.out.println("Goodbye!");
-    }//end main*/
+    public ConnectionFactory(String DB_URL, String USER,String PASS ){
+        this.DB_URL = DB_URL;
+        this.USER = USER;
+        this.PASS = PASS;
+    }
 
 
 }
